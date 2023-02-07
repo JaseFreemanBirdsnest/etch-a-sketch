11f8ea766e2e8ba5270971c09d50f307c28a648f
JaseFreemanBirdsnest <jason.freeman@birdsnest.com.au>
2023-02-07 15:29:02 +1100

    Establish code across HTML, CSS, and JS to build grid of divs, currently hardcoded grid size

---
+++ script.js
@@ -0,0 +1,15 @@
+const gridCont = document.getElementById("grid-container")
+
+
+function buildGrid(col,row){
+    gridCont.style.setProperty('--grid-cols', col);
+    gridCont.style.setProperty('--grid-rows', row);
+    for (i = 0; i < (col*row); i++){
+        let cell = document.createElement("div");
+        cell.innerText = (i + 1);
+        gridCont.appendChild(cell);
+        
+    };
+};
+
+buildGrid(10,10);
