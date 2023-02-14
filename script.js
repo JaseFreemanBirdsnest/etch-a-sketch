862a32d5f19583bcd695e520755152667e233fe2
JaseFreemanBirdsnest <jason.freeman@birdsnest.com.au>
2023-02-14 12:15:10 +1100

    using a video tutorial as a guide, making progress

--- script.js
+++ script.js
@@ -1,17 +1,27 @@
 const gridCont = document.getElementById("grid-container")
-const gridSize = prompt('Please enter a grid size')
+// const gridSize = prompt('Please enter a grid size')
 
-buildGrid(gridSize,gridSize)
+// buildGrid(gridSize)
 
-function buildGrid(col,row){
-    gridCont.style.setProperty('--grid-cols', col);
-    gridCont.style.setProperty('--grid-rows', row);
-    for (i = 0; i < (col*row); i++){
+function buildGrid(size){
+    let removeCells = gridCont.querySelectorAll("div");
+    removeCells.forEach((div) => div.remove);
+
+    gridCont.style.setProperty('--grid-cols', size);
+    gridCont.style.setProperty('--grid-rows', size);
+    for (i = 0; i < (size*size); i++){
         let cell = document.createElement("div");
-        cell.innerText = (i + 1);
+        // cell.innerText = (i + 1);
         cell.setAttribute('class', 'grid-cell');
-
+        cell.addEventListener('mouseover', colourCell);
         gridCont.appendChild(cell);        
     };
 };
   
+function colourCell(){
+    this.style.backgroundColor = 'black';
+}
+
+function changeGridSize(input){
+    buildGrid(input);
+}
